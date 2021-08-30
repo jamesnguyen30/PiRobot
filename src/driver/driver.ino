cb409fab85f5351471ccb9e8550a59aa70256e84
Khuong Nguyen <nguyenthecoder@gmail.com>
2021-08-30 05:20:56 +0100

    Assembled the robot

--- src/driver/driver.ino
+++ src/driver/driver.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include <Servo.h>
 
 int in1 = 2; 
 int in2 = 3;
@@ -7,79 +8,101 @@ int in4 = 5;
 int enA = 10;
 int enB = 11;
 
+int blinkSpeed = 500;
+int cameraServoPin= 6;
+int wheelServoPin= 9;
+
+Servo cameraServo;
+Servo wheelServo;
+
 int i = 0;
 int diff = 100;
-
-int blinkSpeed = 1000;
 
 void stop();
 void goForward(int speed=128);
 void goBackward(int speed = 128);
+void moveCameraServo(int position = 90);
+void moveWheelServoe(int position = 90);
 
 void setup(){
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-    pinMode(in3, OUTPUT);
-    pinMode(in4, OUTPUT);
-    pinMode(enB, OUTPUT);
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
-    stop();
-    // goForward();
+	pinMode(in1, OUTPUT);
+	pinMode(in2, OUTPUT);
+	pinMode(in3, OUTPUT);
+	pinMode(in4, OUTPUT);
+	pinMode(enA, OUTPUT);
+	pinMode(enB, OUTPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, LOW);
+	int pos = 180;
+	cameraServo.attach(cameraServoPin);
+	wheelServo.attach(wheelServoPin);
+	
+	cameraServo.write(pos);
+	wheelServo.write(pos);
 
-    Wire.begin(0x8);
-    Wire.onReceive(receiveEvent);
-    
+	Wire.begin(0x8);
+	Wire.onReceive(receiveEvent);
+	
+	goForward();
+
 }
 void stop(){
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
 	blinkSpeed = 1000;
 }
 
 void goForward(int speed=128){
 
-    digitalWrite(in1,HIGH);
-    digitalWrite(in2,LOW);
-    digitalWrite(in3,HIGH);
-    digitalWrite(in4,LOW);
+	digitalWrite(in1,HIGH);
+	digitalWrite(in2,LOW);
+	digitalWrite(in3,HIGH);
+	digitalWrite(in4,LOW);
 
-    analogWrite(enB,speed);
-    analogWrite(enA,speed);
-	blinkSpeed = 500;
-
+	analogWrite(enB,speed);
+	analogWrite(enA,speed);
+	blinkSpeed = 200;
 }
 
 void goBackward(int speed = 128){
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,HIGH);
-    digitalWrite(in3,LOW);
-    digitalWrite(in4,HIGH);
+	digitalWrite(in1,LOW);
+	digitalWrite(in2,HIGH);
+	digitalWrite(in3,LOW);
+	digitalWrite(in4,HIGH);
 
-    analogWrite(enB,speed);
-    analogWrite(enA,speed);
+	analogWrite(enB,speed);
+	analogWrite(enA,speed);
+	blinkSpeed = 500;
+}
+
+void moveCameraServo(int position = 90){
+	cameraServo.write(position);
+	
+}
+void moveWheelServoe(int position = 90){
+	wheelServo.write(position);
 }
 
 void receiveEvent(int howmany){
-  while(Wire.available()){
-    char c = Wire.read();
-    switch(c){
-      case 0:
-        stop();
-        break;
-      case 1:
-        goForward();
-        break;
-      case 2:
-        goBackward();
-        break;
-      default:
-        stop();
-        break;
-    }
-  }
+	while(Wire.available()){
+		char c = Wire.read();
+		switch(c){
+			case 0:
+				stop();
+				break;
+			case 1:
+				goForward();
+				break;
+			case 2:
+				goBackward();
+				break;
+			default:
+				stop();
+				break;
+		}
+	}
 }
 
 
@@ -88,6 +111,6 @@ void loop(){
 	delay(blinkSpeed);
 	digitalWrite(LED_BUILTIN, LOW);
 	delay(blinkSpeed);
-	
+
 }
 
--- src/test/test.ino
+++ src/test/test.ino
@@ -2,84 +2,118 @@
 #include <Servo.h>
 
 int in1 = 2; 
-int in2 = 3;
-int in3 = 4; 
-int in4 = 5; 
-int enA = 10;
+int in2 = 4;
+int in3 = 7; 
+int in4 = 8; 
+int enA = 6;
 int enB = 11;
 int blinkSpeed = 500;
-int cameraServoPin= 6;
-int wheelServoPin= 9;
+int cameraServoPin= 3;
+int wheelServoPin= 5;
+
+int pos = 0;
 
 void goForward(int speed=128);
+void stop();
+void turn(int position=90);
 
 Servo cameraServo;
 Servo wheelServo;
 
-int pos = 180;
 
 void setup(){
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-    pinMode(in3, OUTPUT);
-    pinMode(in4, OUTPUT);
-    pinMode(enB, OUTPUT);
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
-    goForward();
-    Wire.begin(0x8);
-    Wire.onReceive(receiveEvent);
+	pinMode(in1, OUTPUT);
+	pinMode(in2, OUTPUT);
+	pinMode(in3, OUTPUT);
+	pinMode(in4, OUTPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, LOW);
+	Wire.begin(0x8);
+	Wire.onReceive(receiveEvent);
 
-    cameraServo.attach(cameraServoPin);
-    wheelServo.attach(wheelServoPin);
-    cameraServo.write(pos);
-    wheelServo.write(pos);
+	int pos = 90;
+	cameraServo.attach(cameraServoPin);
+	wheelServo.attach(wheelServoPin);
+
+	goForward();
 
 
-    
 }
 void goForward(int speed=128){
 
-    digitalWrite(in1,HIGH);
-    digitalWrite(in2,LOW);
-    digitalWrite(in3,HIGH);
-    digitalWrite(in4,LOW);
+	digitalWrite(in1,HIGH);
+	digitalWrite(in2,LOW);
+	digitalWrite(in3,HIGH);
+	digitalWrite(in4,LOW);
 
-    analogWrite(enB,speed);
-    analogWrite(enA,speed);
+	analogWrite(enB,speed);
+	analogWrite(enA,speed);
+	analogWrite(9,speed);
+	analogWrite(6,speed);
+	blinkSpeed = 500;
 
+}
+
+void stop(){
+	digitalWrite(in1,LOW);
+	digitalWrite(in2,LOW);
+	digitalWrite(in3,LOW);
+	digitalWrite(in4,LOW);
+	analogWrite(enB,0);
+	analogWrite(enA,0);
+}
+
+void turn(int pos = 90){
+	wheelServo.write(pos);
+}
+
+void shake(int pos = 90){
+	cameraServo.write(pos);
 }
 
 void receiveEvent(int howmany){
-  while(Wire.available()){
-    char c = Wire.read();
-    switch(c){
-      case 0:
-        blinkSpeed=1000;
-        break;
-			
-      default:
-        blinkSpeed=500;
-        break;
-    }
-  }
+	while(Wire.available()){
+		char c = Wire.read();
+		switch(c){
+			case 0:
+				blinkSpeed=1000;
+				stop();
+				break;
+			case 1: 
+				goForward();
+				break;
+			case 2: 
+				turn(pos);
+				shake(pos);
+				if(pos == 0){
+					pos = 180;
+				} else {
+					pos = 0;
+				}
+
+				break;
+			default:
+				blinkSpeed=500;
+				break;
+		}
+	}
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(blinkSpeed);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(blinkSpeed);
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(blinkSpeed);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(blinkSpeed);
 
 
 
-  // for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-  //   cameraServo.write(pos);              // tell servo to go to position in variable 'pos'
-  //   delay(15);                       // waits 15ms for the servo to reach the position
-  // }
-  // for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-  //   cameraServo.write(pos);              // tell servo to go to position in variable 'pos'
-  //   delay(15);                       // waits 15ms for the servo to reach the position
-  // }
+	// for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+	//   cameraServo.write(pos);              // tell servo to go to position in variable 'pos'
+	//   delay(15);                       // waits 15ms for the servo to reach the position
+	// }
+	// for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+	//   cameraServo.write(pos);              // tell servo to go to position in variable 'pos'
+	//   delay(15);                       // waits 15ms for the servo to reach the position
+	// }
 
 }
